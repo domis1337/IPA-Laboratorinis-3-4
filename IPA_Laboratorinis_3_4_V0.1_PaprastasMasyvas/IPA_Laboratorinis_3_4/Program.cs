08b69e4156187491abba697b75ce61465de57286
domis1337 <52240763+domis1337@users.noreply.github.com>
2020-03-18 16:42:11 +0200

    Add files via upload

---
+++ IPA_Laboratorinis_3_4_V0.1_PaprastasMasyvas/IPA_Laboratorinis_3_4/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IPA_Laboratorinis_3_4
+{
+    class Program
+    {
+
+        static List<Studentas> studentai = new List<Studentas>();
+        static int Main(string[] args)
+        {
+
+            
+
+
+            Menu();
+
+            return 0;
+        } 
+
+
+        static void Menu()
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Ivesti nauja studenta");
+            Console.WriteLine("2. Spausdinti duomenis (vid)");
+            Console.WriteLine("3. Spausdinti duomenis (med)");
+            Console.WriteLine("4. Baigti darba");
+            Console.WriteLine("\nPasirinkimas:");
+            string a = Console.ReadLine();
+
+            switch (a)
+            {
+                case "1":
+                    IvestiStudenta();
+                    break;
+
+                case "2":
+                    DuomenysVid();
+                    break;
+
+                case "3":
+                    DuomenysMed();
+                    break;
+
+                case "4":
+                    break;
+
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+
+        static void IvestiStudenta()
+        {
+            string vardas;
+            string pavarde;
+            int[] nd_rez = new int[1];
+            int egz_rez;
+
+
+            Console.WriteLine("Enter students name: ");
+            vardas = Console.ReadLine();
+            Console.WriteLine("Enter students last name: ");
+            pavarde = Console.ReadLine();
+
+            string ats;
+
+            Console.WriteLine("Enter 'r' if you want to randomly generate students grades:");
+            ats = Console.ReadLine();
+
+
+            if (ats == "r")
+            {
+                Random rnd = new Random();
+                Array.Resize(ref nd_rez, 6);
+                nd_rez[0] = rnd.Next(1, 11);
+                nd_rez[1] = rnd.Next(1, 11);
+                nd_rez[2] = rnd.Next(1, 11);
+                nd_rez[3] = rnd.Next(1, 11);
+                nd_rez[4] = rnd.Next(1, 11);
+                nd_rez[5] = rnd.Next(1, 11);
+                egz_rez = rnd.Next(1, 11);
+            }
+            else
+            {
+                for (int i = 0; i <= nd_rez.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.WriteLine("Enter a homework grade: ");
+                        int.TryParse(Console.ReadLine(), out egz_rez);
+                        Array.Resize(ref nd_rez, nd_rez.Length + 1);
+                        nd_rez[0] = egz_rez;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter another homework grade?(Y/N)");
+                        ats = Console.ReadLine();
+                        if (ats == "Y")
+                        {
+                            Console.WriteLine("Enter grade: ");
+                            int.TryParse(Console.ReadLine(), out egz_rez);
+                            Array.Resize(ref nd_rez, nd_rez.Length + 1);
+                            nd_rez[0] = egz_rez;
+                        }
+                        else if (ats == "N")
+                        {
+                            Array.Resize(ref nd_rez, nd_rez.Length - 1);
+                            break;
+                        }
+                        else
+                        {
+                            i--;
+                        }
+                    }
+
+                }
+
+
+                Console.WriteLine("Enter students exam grade: ");
+                int.TryParse(Console.ReadLine(), out egz_rez);
+            }
+
+            
+
+
+            studentai.Add(new Studentas(vardas, pavarde, nd_rez, egz_rez));
+
+            Menu();
+
+        }
+
+
+        static void DuomenysVid()
+        {
+            Console.WriteLine("Vardas".PadRight(15, ' ') + "Pavarde".PadRight(20, ' ') + "Galutinis (Vid)");
+            Console.WriteLine("".PadRight(50, '-'));
+
+            foreach (Studentas studentas in studentai)
+            {
+                Console.WriteLine(studentas.vardas.PadRight(15, ' ') + studentas.pavarde.PadRight(20, ' ') + (String.Format("{0:0.00}", studentas.galutinis_vid)).PadLeft(15,' '));
+            }
+
+            Console.WriteLine("\n\n");
+
+            Menu();
+        }
+
+
+        static void DuomenysMed()
+        {
+            Console.WriteLine("Vardas".PadRight(15, ' ') + "Pavarde".PadRight(20, ' ') + "Galutinis (Med)");
+            Console.WriteLine("".PadRight(50, '-'));
+
+            foreach (Studentas studentas in studentai)
+            {
+                Console.WriteLine(studentas.vardas.PadRight(15, ' ') + studentas.pavarde.PadRight(20, ' ') + (String.Format("{0:0.00}", studentas.galutinis_med)).PadLeft(15, ' '));
+            }
+
+            Console.WriteLine("\n\n");
+
+            Menu();
+        }
+
+    }
+}
---
+++ IPA_Laboratorinis_3_4_V0.1_PaprastasMasyvas/IPA_Laboratorinis_3_4/Studentas.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPA_Laboratorinis_3_4
+{
+    class Studentas
+    {
+
+        public string vardas;
+        public string pavarde;
+        public int[] nd_rez;
+        public int egz_rez;
+        public double galutinis_vid;
+        public double galutinis_med;
+
+
+        public Studentas(string vardas, string pavarde, int[] nd_rez, int egz_rez)
+        {
+            this.vardas = vardas;
+            this.pavarde = pavarde;
+            this.nd_rez = nd_rez;
+            this.egz_rez = egz_rez;
+
+            double a = 0;
+
+            for (int i = 0; i < nd_rez.Length; i++)
+            {
+                a += nd_rez[i];
+            }
+
+            a = a / nd_rez.Length;
+
+            a = a * 0.3 + egz_rez * 0.7;
+
+            this.galutinis_vid = a;
+
+            Array.Sort(nd_rez);
+
+            if (nd_rez.Length % 2 == 0)
+            {
+                a = (nd_rez[(nd_rez.Length/2)-1] + nd_rez[(nd_rez.Length / 2)]) / 2;
+            }
+            else
+            {
+                a = nd_rez[nd_rez.Length/2];
+            }
+
+            
+            a = a * 0.3 + egz_rez * 0.7;
+
+            this.galutinis_med = a;
+
+        }
+
+      
+    }
+}
