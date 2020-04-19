d196120c5aa4e04a32170a1f381b42dc845a4706
domis1337 <52240763+domis1337@users.noreply.github.com>
2020-04-19 19:40:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IPA_Laboratorinis_3_4
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+            var instance = new Funkcijos();
+            instance.Menu();
+
+            return 0;
+        }
+
+
+    }
+}
